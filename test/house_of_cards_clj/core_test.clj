60162732db6c1ca98d63d4e08ce8a50404da5333
Abhinav Sharma <abhi18av@gmail.com>
2018-10-25 13:55:36 +0530

    initial commit

---
+++ test/house_of_cards_clj/core_test.clj
@@ -0,0 +1,7 @@
+(ns house-of-cards-clj.core-test
+  (:require [clojure.test :refer :all]
+            [house-of-cards-clj.core :refer :all]))
+
+(deftest a-test
+  (testing "FIXME, I fail."
+    (is (= 0 1))))
